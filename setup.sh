ac814a3e0557cf998002de4264e7ce6414b8e63d
jaml <dev@jaml.site>
2015-10-23 11:10:29 +0100

    Mount includes and other improvements

--- setup.sh
+++ setup.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
-set -e
 
-while getopts "p:n:e:i:d" OPT
+WEB_PATH='./data/ruby/web'
+WEB_INCLUDES_PATH='./data/ruby/includes'
+
+mount_web_folder() {
+	echo " » Mounting '$1' into '$2'."
+	echo "sudo mount -o bind $1 $2"
+	# sudo mount -o bind $1 $2
+
+	if [[ $3 = 1 ]]; then
+		echo " » Remounting as read-only."
+		echo "sudo mount -o remount,ro $2"
+		# sudo mount -o remount,ro $2
+	fi
+}
+
+# TODO remove ro?
+# TODO after reboot / running more than once without options?
+
+while getopts ":p:n:e:i:rd" OPT
 do
   case $OPT in
     p) WEB_PROJECT=$OPTARG;;
     n) WEB_NAME=$OPTARG;;
     e) WEB_ENVIRONMENT=$OPTARG;;
-    i) WEB_INCLUDES=$OPTARG;;
+	i) WEB_INCLUDES+=("$OPTARG");;
+	r) WEB_MOUNT_RO=1;;
     d) WEB_DAEMON=1;;
-    *) echo "Unknown argument";;
+
+	:) echo "Option -$OPTARG requires an argument" >&2
+	   exit 1;;
+    *) echo "Unknown option" >&2
+       exit 1;;
   esac
 done
 
-echo $WEB_INCLUDES
-
 if [[ -z $WEB_PROJECT ]]; then
-  echo "Project path is required"
-  exit 1
+  echo "Project path is required" >&2
+  exit 2
 fi
 
 if [[ -z $WEB_ENVIRONMENT ]]; then
@@ -30,27 +50,47 @@ fi
 
 if [[ ! -d "./environments/$WEB_ENVIRONMENT" ]]; then
 
-  echo "Environment '$WEB_ENVIRONMENT' does not exist"
-  exit 2
-
+  echo "Environment '$WEB_ENVIRONMENT' does not exist" >&2
+  exit 3
 else
 
   if [[ ! -e "./environments/$WEB_ENVIRONMENT/docker-compose.yml" ]]; then
-    echo "Environment '$WEB_ENVIRONMENT' does not contain a 'docker-compose.yml' file"
+    echo "Environment '$WEB_ENVIRONMENT' does not contain a 'docker-compose.yml' file" >&2
     exit 3
   fi
 fi
 
-# if mount | grep
+if [[ ! -z `mount | grep $WEB_PROJECT".*"$WEB_PATH" .*bind"` ]]; then
+	echo " » '$WEB_PROJECT' is already mounted on '$WEB_PATH'."
 
-echo "sudo mount -o bind $WEB_PROJECT ./web/main/"
-# echo "sudo umount $WEB_PROJECT"
+else
+  IS_WEB_MOUNTED=`mount | grep .*$WEB_PATH" .*bind"`
 
-echo "docker-compose -p $WEB_NAME -f ./environments/$WEB_ENVIRONMENT/docker-compose.yml up -d"
-# docker-compose -p $WEB_NAME -f ./environments/$WEB_ENVIRONMENT/docker-compose.yml up
+  if [[ ! -z $IS_WEB_MOUNTED ]]; then
+	echo " » Something different than '$WEB_PROJECT' is mounted on $WEB_PATH:"
+	echo -e "\n\t$IS_WEB_MOUNTED\n"
+	echo " » It is possible to umount it, but I, a humble shell script, I am not going to mess with you:"
+	echo -e "\n\tsudo umount $WEB_PATH\n"
+	exit 4
 
-# shift $($# -1)
+  else
+  	  mount_web_folder $WEB_PROJECT $WEB_PATH $WEB_MOUNT_RO
+  fi
+fi
 
-echo $1
+if [[ ! -z $WEB_INCLUDES ]]; then
+  for WEB_INCLUDE in "${WEB_INCLUDES[@]}"; do
+    WEB_INCLUDE_FOLDER=${WEB_INCLUDE##*/}
+    mount_web_folder $WEB_INCLUDE $WEB_INCLUDES_PATH/$WEB_INCLUDE_FOLDER $WEB_MOUNT_RO
+  done
+fi
 
-# echo "Others $(@)"
+echo -e "\n » Running Docker Compose"
+
+DOCKER_COMPOSE_COMMAND="docker-compose -p $WEB_NAME -f ./environments/$WEB_ENVIRONMENT/docker-compose.yml up"
+
+if [[ $WEB_DAEMON = 1 ]]; then
+	DOCKER_COMPOSE_COMMAND+=' -d'
+fi
+
+echo  $DOCKER_COMPOSE_COMMAND
